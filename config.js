d7137da8e6a4e2a37cbbe6e367e1401d2d9bcebf
Vijay <vijayjs2208@gmail.com>
2018-01-27 16:00:13 -0800

    Making Change to config

--- config.js
+++ config.js
@@ -9,5 +9,5 @@ let config = {
 module.exports = {
     databaseUrl: process.env.databaseUrl || global.databaseUrl || config.databaseConnection,
     port: process.env.PORT || config.serverPort,
-    testdatabaseURl: process.env.testdatabaseURl || 'mongodb://localhost/test-Blog'
+    testdatabaseURl: process.env.testdatabaseURl || 'mongodb://vijayjs2208:vijayjs2208@ds115768.mlab.com:15768/blog' || 'mongodb://localhost/test-Blog'
 };
